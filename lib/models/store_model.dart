d9a8f2fcef3872b4ae328a4a4a5766e5bca230c4
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2023-11-02 10:27:16 +0700

    small db adjustment

--- lib/models/store_model.dart
+++ lib/models/store_model.dart
@@ -1,7 +1,6 @@
 class StoreModel {
   int id;
   String storeName;
-  String storeLocation;
   String thumbnail;
   String email;
   String userId;
@@ -9,7 +8,6 @@ class StoreModel {
   StoreModel({
     required this.id,
     required this.storeName,
-    required this.storeLocation,
     required this.thumbnail,
     required this.email,
     required this.userId,
@@ -18,7 +16,6 @@ class StoreModel {
   factory StoreModel.fromJson(Map<String, dynamic> json) => StoreModel(
         id: json['id'],
         storeName: json['store_name'],
-        storeLocation: json['store_location'],
         thumbnail: json['thumbnail'],
         email: json['email'],
         userId: json['user_id'],
