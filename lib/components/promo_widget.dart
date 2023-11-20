302c23f23fd58afb2a328041266f5e823e33eb98
MaulanaArya30 <maulanaaryaw@gmail.com>
2023-11-20 18:42:09 +0700

    popuppromo

--- lib/components/promo_widget.dart
+++ lib/components/promo_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_menu/components/constants.dart';
 
 import '../models/promos_model.dart';
 
@@ -14,6 +15,137 @@ class PromoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2,
+      child: PopUpPromo(promo: promo),
+    );
+  }
+}
+
+class PopUpPromo extends StatelessWidget {
+  const PopUpPromo({
+    super.key,
+    required this.promo,
+  });
+
+  final Promo promo;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  color: AppColor.backgroundColor, //theme.card,
+                  borderRadius: BorderRadius.circular(22),
+                ),
+                height: double.infinity,
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 9,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          image: DecorationImage(
+                            image: NetworkImage(promo.thumbnail ?? ""),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    // Expanded(
+                    //   flex: 2,
+                    //   child: Column(
+                    //     crossAxisAlignment: CrossAxisAlignment.center,
+                    //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //     children: [
+                    //       Row(
+                    //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //         children: [
+                    //           Text(
+                    //             menu.title ?? "",
+                    //             style: TextStyle(
+                    //               color: theme.foreground,
+                    //               fontSize: 32,
+                    //               fontWeight: FontWeight.w500,
+                    //             ),
+                    //           ),
+                    //           Text(
+                    //             'Rp. ${menu.price}',
+                    //             style: TextStyle(
+                    //               color: theme.foreground,
+                    //               fontSize: 32,
+                    //               fontWeight: FontWeight.w500,
+                    //             ),
+                    //           ),
+                    //         ],
+                    //       ),
+                    //       Expanded(
+                    //         flex: 2,
+                    //         child: Container(
+                    //           padding: const EdgeInsets.only(top: 20),
+                    //           child: SingleChildScrollView(
+                    //             child: Text(
+                    //               menu.description ?? "",
+                    //               textAlign: TextAlign.start,
+                    //               style: TextStyle(
+                    //                 color: theme.foreground.withOpacity(0.6),
+                    //                 fontSize: 20,
+                    //                 fontWeight: FontWeight.w500,
+                    //               ),
+                    //             ),
+                    //           ),
+                    //         ),
+                    //       ),
+                    //       Expanded(
+                    //         flex: 2,
+                    //         child: Container(
+                    //           alignment: Alignment.bottomCenter,
+                    //           child: InkWell(
+                    //             onTap: () => Navigator.pop(context),
+                    //             child: Text(
+                    //               'Back',
+                    //               style: TextStyle(
+                    //                   fontSize: 24, color: theme.primary),
+                    //             ),
+                    //           ),
+                    //         ),
+                    //       ),
+                    //     ],
+                    //   ),
+                    // ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        child: InkWell(
+                          onTap: () => Navigator.pop(context),
+                          child: Text(
+                            'Back',
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: AppColor.buttonColor), //theme.primary),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 15),
         padding: const EdgeInsets.all(25),
